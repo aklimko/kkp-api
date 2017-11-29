01cb607c1780e1201f50327b0d7ab8a20a474521
Adam Klimko <exusar@gmail.com>
2017-11-29 21:39:44 +0100

    Removed unnecessary service

--- src/main/java/pl/adamklimko/kkp/service/ProductsEntryService.java
+++
@@ -1,8 +0,0 @@
-package pl.adamklimko.kkp.service;
-
-import pl.adamklimko.kkp.model.products.Products;
-
-public interface ProductsEntryService {
-    Iterable<Products> find();
-    void save(Products boughtProducts);
-}
