189a67aba1e5bb916fe6b56a22064d191d4ced2c
Adam Klimko <exusar@gmail.com>
2018-05-19 22:22:24 +0200

    Get rid of ApiError class

--- src/main/java/pl/adamklimko/kkp/error/RestExceptionHandler.java
+++ src/main/java/pl/adamklimko/kkp/error/RestExceptionHandler.java
@@ -1,7 +1,6 @@
 package pl.adamklimko.kkp.error;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +9,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import pl.adamklimko.kkp.error.exceptions.ServiceException;
 import pl.adamklimko.kkp.error.exceptions.UserNotFoundException;
 import pl.adamklimko.kkp.error.exceptions.UsernameAlreadyExistsException;
 
@@ -29,14 +29,21 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
   @Override
   protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
       HttpHeaders headers, HttpStatus status, WebRequest request) {
-    List<String> errors = new ArrayList<>();
+    String errorMessage = parseErrorMessage(ex);
+
+    HttpStatus badRequestStatus = HttpStatus.BAD_REQUEST;
+    JsonNode formattedResponse =
+        new ServiceException(badRequestStatus, errorMessage).formattedResponse();
+    return handleExceptionInternal(ex, formattedResponse, headers, badRequestStatus, request);
+  }
+
+  private String parseErrorMessage(MethodArgumentNotValidException ex) {
+    String[] errorMessage = new String[]{""};
 
     ex.getBindingResult().getFieldErrors().forEach(error ->
-        errors.add(error.getField() + ": " + error.getDefaultMessage()));
+        errorMessage[0] += error.getField() + ": " + error.getDefaultMessage());
     ex.getBindingResult().getGlobalErrors().forEach(error ->
-        errors.add(error.getObjectName() + ": " + error.getDefaultMessage()));
-
-    ApiError apiError = new ApiError(errors);
-    return handleExceptionInternal(ex, apiError, headers, HttpStatus.BAD_REQUEST, request);
+        errorMessage[0] += error.getObjectName() + ": " + error.getDefaultMessage());
+    return errorMessage[0];
   }
 }
