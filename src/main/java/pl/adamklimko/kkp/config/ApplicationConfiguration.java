c6e6d4a97d901aafb7933806016f63c8886ddae6
Adam Klimko <exusar@gmail.com>
2018-05-19 21:30:13 +0200

    Move beans declarations into one class

--- src/main/java/pl/adamklimko/kkp/config/ApplicationConfiguration.java
+++ src/main/java/pl/adamklimko/kkp/config/ApplicationConfiguration.java
@@ -3,9 +3,15 @@ package pl.adamklimko.kkp.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-public class ModelMapperConfiguration {
+public class ApplicationConfiguration {
+
+  @Bean
+  public BCryptPasswordEncoder bCryptPasswordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 
   @Bean
   public ModelMapper modelMapper() {
