c0d7ea30b4d17b6cd22a620a1c920172a4f90894
Adam Klimko <exusar@gmail.com>
2018-05-25 00:05:13 +0200

    Add group dto tests

--- src/test/java/pl/adamklimko/kkp/model/dto/GroupDtoTest.java
+++ src/test/java/pl/adamklimko/kkp/model/dto/GroupDtoTest.java
@@ -1,33 +1,101 @@
 package pl.adamklimko.kkp.model.dto;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import javax.validation.ConstraintViolation;
-import org.hibernate.validator.HibernateValidator;
-import org.junit.Before;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import pl.adamklimko.kkp.model.UserData;
 
 class GroupDtoTest {
 
-  private LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
+  private Validator validator;
 
-  @Before
-  public void setup() {
-    localValidatorFactoryBean.setProviderClass(HibernateValidator.class);
-    localValidatorFactoryBean.afterPropertiesSet();
+  private GroupDto groupDto;
+
+  public GroupDtoTest() {
+    setup();
+  }
+
+  private void setup() {
+    validator = Validation.buildDefaultValidatorFactory().getValidator();
+  }
+
+  @BeforeEach
+  public void init() {
+    groupDto = new GroupDto();
   }
 
   @ParameterizedTest
   @CsvSource({
-    "Aredos, true"
+      "Aredos, true",
+      "_, false",
+      "a, false",
+      "abc, true",
+      "VeryLongNameForAGroupLike, false",
+      "VeryLongNameForAGroupLik, true",
+      "user_name, true",
+      "_username, false",
+      "MyUniqueName$, false",
+      "Name with spaces, false",
+      "   , false"
   })
-  public void testUsernameValidation(String name, boolean valid) {
-    GroupDto groupDto = new GroupDto();
+  public void testNameValidation(String name, boolean valid) {
     groupDto.setName(name);
-    Set<ConstraintViolation<GroupDto>> constraints = localValidatorFactoryBean.validate(groupDto, GroupDto.class);
-    assertEquals(constraints.size() == 0, valid);
+    groupDto.setUsers(getGroupUsers());
+
+    Set<ConstraintViolation<GroupDto>> constraints = validator.validate(groupDto);
+    String message =
+        String.format("Name: '%s' should be %s", name, valid ? "valid" : "not valid");
+    assertEquals(valid, constraints.size() == 0, message);
+  }
+
+  private List<UserData> getGroupUsers() {
+    List<UserData> users = new ArrayList<>();
+    users.add(UserData.builder()
+        .username("valid-username")
+        .role("USER")
+        .build());
+    return users;
+  }
+
+  @Test
+  public void testGroupUsersNull() {
+    groupDto.setName("ValidName");
+
+    Set<ConstraintViolation<GroupDto>> constraints = validator.validate(groupDto);
+
+    assertFalse(constraints.size() == 0);
+  }
+
+  @Test
+  public void testGroupUsersEmpty() {
+    groupDto.setName("ValidName");
+    groupDto.setUsers(new ArrayList<>());
+
+    Set<ConstraintViolation<GroupDto>> constraints = validator.validate(groupDto);
+
+    assertFalse(constraints.size() == 0);
+  }
+
+  @Test
+  public void testGroupUsersWithOneUser() {
+    groupDto.setName("ValidName");
+    ArrayList<UserData> users = new ArrayList<>();
+    users.add(UserData.builder().username("ValidName").role("USER").build());
+    groupDto.setUsers(users);
+
+    Set<ConstraintViolation<GroupDto>> constraints = validator.validate(groupDto);
+
+    assertTrue(constraints.size() == 0);
   }
 }
