21bc1a599e92e4ba35a7509cfbda08732e43e473
Adam Klimko <exusar@gmail.com>
2018-05-30 17:33:07 +0200

    Improve service test

--- src/test/java/pl/adamklimko/kkp/service/UserServiceTest.java
+++ src/test/java/pl/adamklimko/kkp/service/UserServiceTest.java
@@ -1,32 +1,61 @@
 package pl.adamklimko.kkp.service;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import java.util.ArrayList;
 import java.util.List;
-import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import pl.adamklimko.kkp.model.dto.UserDto;
 import pl.adamklimko.kkp.model.entity.UserEntity;
 import pl.adamklimko.kkp.repository.UserRepository;
 
 class UserServiceTest {
 
+  @InjectMocks
+  private UserService userService;
+  @InjectMocks
+  private BCryptPasswordEncoder bCryptPasswordEncoder;
+  @InjectMocks
+  private ModelMapper modelMapper;
+
   @Mock
   private UserRepository userRepository;
 
-  @Before
-  public void setup() {
+  public UserServiceTest() {
     MockitoAnnotations.initMocks(this);
+    userService = new UserService(userRepository, bCryptPasswordEncoder, modelMapper);
+  }
+
+  @BeforeEach
+  public void setup() {
+    init();
   }
 
   @Test
-  void insertUser() {
+  public void insertUser() {
+    UserDto userDto = new UserDto("test", "testpassword");
+    userService.insertUser(userDto);
+    userService.getUser("test");
   }
 
   @Test
-  void getUser() {
-    List<UserEntity> users = userRepository.findAll();
-    assertEquals(0, users.size());
+  public void getUser() {
+
+  }
+
+  private void init() {
+    Mockito.when(userRepository.findAll()).thenReturn(getAllUsers());
+  }
+
+  private List<UserEntity> getAllUsers() {
+    List<UserEntity> users = new ArrayList<>();
+    users.add(new UserEntity("1","test","password"));
+    users.add(new UserEntity("2","exuso","password"));
+    return users;
   }
 }
